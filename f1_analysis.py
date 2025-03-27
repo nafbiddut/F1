c48ce099b41ade607c25f9430842c2284aa481d9
nafbiddut <2020332@iub.edu.bd>
2025-03-27 15:30:09 +0600

    updated

--- f1_analysis.py
+++ f1_analysis.py
@@ -1,1 +1,11 @@
-print("Hello, F1 Data Science!")
+import fastf1
+print("FastF1 is installed and working!")
+
+session = fastf1.get_session(2024, 'Monza', 'Race')
+session.load()
+
+df = session.laps
+
+
+
+print(df)
